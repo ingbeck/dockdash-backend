68e33e8fd890d7bcd047761d696d37f7e3b2a875
ingobecker <mail@ingobecker.de>
2024-05-21 15:52:36 +0200

    inital commit

---
+++ src/main/java/com/ingbeck/dockdashbackend/DockdashBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ingbeck.dockdashbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockdashBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockdashBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ingbeck/dockdashbackend/DockdashBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingbeck.dockdashbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockdashBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
